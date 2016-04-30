c5c3c39f3bfb4f689fa3ede5e909e5030b102775
Junejie <junejieruzol1@gmail.com>
2016-05-01 03:32:54 +0800

    proc item added

--- state/pipelines.py
+++ state/pipelines.py
@@ -11,8 +11,3 @@ class StatePipeline(object):
     def process_item(self, item, spider):
         print 'xxx >> ',item
         return item
-
-    def get_media_requests(self, item, info):
-        for image_url in item['image_urls']:
-            print 'image_url:', image_url
-            yield Request(image_url)
